b3579e517d01d2b47e77a68efab083525b2fed39
sashavitalevich92-beepp <sasha.vitalevich.92@gmail.com>
2026-02-10 08:12:59 +0300

    Final configuration for TimeWeb Flask app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-﻿web: gunicorn main:app --bind 0.0.0.0:
+﻿web: gunicorn wsgi:app --bind 0.0.0.0:$PORT
--- app.py
+++
@@ -1,6 +0,0 @@
-﻿import subprocess
-import time
-
-if __name__ == '__main__':
-    # Просто запускаем бота
-    subprocess.run(["python", "bot.py"])
